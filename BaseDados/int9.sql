8311de8386169516ab5f17355c0a73b781c2f7b3
Miguel Pinto <pt.miguel99@gmail.com>
2019-05-26 14:51:38 +0100

    Fixed int9

--- BaseDados/int9.sql
+++ BaseDados/int9.sql
@@ -2,8 +2,9 @@
 .mode columns
 .headers on
 .nullvalue NULL
+.width 20
 
-SELECT Nome, count(*)
+SELECT Nome, count(*) as numRequisicoes
 FROM Pessoa
 JOIN Requisicao
 ON Requisicao.ccUtilizador = Pessoa.cartaoCidadao
